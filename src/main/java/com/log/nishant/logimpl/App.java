a760c4b1681ff27034ed84d12d9d704d586da39d
Nishant Kumar Singh <52786028+nishu9162306344@users.noreply.github.com>
2022-10-25 23:29:17 +0530

    add

---
+++ src/main/java/com/log/nishant/logimpl/App.java
@@ -0,0 +1,13 @@
+package com.log.nishant.logimpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/log/nishant/logimpl/TestDemo.java
@@ -0,0 +1,44 @@
+package com.log.nishant.logimpl;
+
+import java.io.IOException;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.HTMLLayout;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.xml.XMLLayout;
+
+public class TestDemo {
+
+	//define Logger and create the logger object
+
+	private static Logger logger=Logger.getLogger(TestDemo.class);
+
+	//main method
+	public static void main(String[] args) throws IOException {
+
+		//create the layout for the logger 
+		//Layout layout=new HTMLLayout();
+		//Layout layout=new SimpleLayout();
+		//Layout layout=new XMLLayout();
+		Layout layout=new PatternLayout("%d %t %C %M %m %p %n");
+
+
+
+		//create the appender and link the layout
+		Appender appender=new FileAppender(layout,"D:/logs.log");
+		//link appender with logger 
+		logger.addAppender(appender);
+		//print messages 
+		logger.debug("From debug");
+		logger.info("from appender");
+		logger.warn("from warn");
+		logger.error("from error");
+		logger.fatal("from fatal");
+
+	}
+}
